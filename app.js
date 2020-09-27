de56d7a86aa0858ff520b246548163e3ed7c3e63
Kaikai8888 <wuky812@gmail.com>
2020-09-27 19:04:30 +0800

    Init: add index page to show movie list

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+const restaurants = require('./restaurant.json')
+const restaurantsObject = restaurants.results.reduce((acc, cur) => {
+  acc[cur.id] = cur
+  return acc
+}, {})
+
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
+
+//setting routes
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurants.results })
+})
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
