9b06b37d029bd84d54a8f5ba651cf798faa0dbf0
Kaikai8888 <wuky812@gmail.com>
2020-09-30 19:36:02 +0800

    feat: modify index page data source hide show page and search function route temporary to focus on index page testing

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-//require related packages
+//require related packages and modules
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant.js')
 //server related variables
 const app = express()
 const port = 3000
 
-const restaurants = require('./restaurant.json')
-const restaurantsObject = restaurants.results.reduce((object, restaurant) => {
-  object[restaurant.id] = restaurant
-  return object
-}, {})
+// const restaurants = require('./restaurant.json')
+// const restaurantsObject = restaurants.results.reduce((object, restaurant) => {
+//   object[restaurant.id] = restaurant
+//   return object
+// }, {})
 
 
 //database setting
@@ -23,26 +24,30 @@ db.once('open', () => console.log('MongoDB connect!'))
 //setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 //setting static files
 app.use(express.static('public'))
 
 //setting routes
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurants.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      res.render('index', { restaurants })
+    })
+    .catch(error => console.error(error))
 })
 
-app.get('/restaurants/:id', (req, res) => {
-  res.render('show', { restaurant: restaurantsObject[req.params.id] })
-})
+// app.get('/restaurants/:id', (req, res) => {
+//   res.render('show', { restaurant: restaurantsObject[req.params.id] })
+// })
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim().toLowerCase()
-  const searchResults = restaurants.results.filter(item => item.name.toLowerCase().includes(keyword))
-  res.render('index', { restaurants: searchResults, keyword })
-})
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword.trim().toLowerCase()
+//   const searchResults = restaurants.results.filter(item => item.name.toLowerCase().includes(keyword))
+//   res.render('index', { restaurants: searchResults, keyword })
+// })
 
-
+//run web server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
