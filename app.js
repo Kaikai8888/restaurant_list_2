2a11db99b88d76b33db99a5e0aff2443a679f672
Kaikai8888 <wuky812@gmail.com>
2020-09-30 18:37:32 +0800

    feat: add seed data

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const restaurantsObject = restaurants.results.reduce((object, restaurant) => {
 
 
 //database setting
-mongoose.connect('mongodb://localhost/restaurant-list')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => console.log('MongoDB error!'))
 db.once('open', () => console.log('MongoDB connect!'))
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,19 @@
+//require packages and data
+const restaurantSeeds = require('../../restaurant.json').results
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant.js')
+
+//data base connection setting
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => console.log('MongoDB error!'))
+db.once('open', () => {
+  console.log('MongoDB connect!')
+  //store seed data into database
+  restaurantSeeds.forEach(seed => {
+    delete seed.id
+    Restaurant.create(seed)
+  })
+  console.log('Complete creating seed data')
+})
