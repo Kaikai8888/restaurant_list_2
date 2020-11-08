44e68d0d613a207f5592b3d505ba981c73457c95
Kaikai8888 <wuky812@gmail.com>
2020-11-08 21:56:47 +0800

    feat: add authenticator to home and restaurants route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const home = require('./modules/home.js')
 const users = require('./modules/users.js')
 const restaurants = require('./modules/restaurants.js')
+const { authenticator } = require('../middleware/auths.js')
 
 router.use('/users', users)
-router.use('/restaurants', restaurants)
-router.use('/', home)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/', authenticator, home)
 
 module.exports = router
