215c1287929e0465efdd8c7739980ef9fa669ff4
Kaikai8888 <wuky812@gmail.com>
2020-11-08 16:18:59 +0800

    feat: add login page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home.js')
+const users = require('./modules/users.js')
 const restaurants = require('./modules/restaurants.js')
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
+router.use('/', home)
 
 module.exports = router
