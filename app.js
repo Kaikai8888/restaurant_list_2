54025f99fb32cd65099e9e11b0343d724373c5e5
Kaikai8888 <wuky812@gmail.com>
2020-11-08 21:22:36 +0800

    feat: add express session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const router = require('./routes')
 require('./config/mongoose.js')
@@ -19,6 +20,11 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: "djsiourni",
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(router)
 
 //run web server
